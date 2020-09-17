b0e4119f3abc08fbeb5a1d27d6c2b49de7f97b66
Dmitry Dalinger <dmitry.dalinger@nordigy.ru>
2020-09-17 18:22:20 +0300

    Lecture 4.8

--- addressbook-web-tests/src/test/java/ru/disanger/learning/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/disanger/learning/addressbook/model/GroupData.java
@@ -1,49 +1,24 @@
 package ru.disanger.learning.addressbook.model;
 
 public class GroupData {
-    private final String groupID;
+    private  int groupID;
     private final String groupName;
     private final String groupHeader;
     private final String groupFooter;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        GroupData groupData = (GroupData) o;
-
-        if (groupID != null ? !groupID.equals(groupData.groupID) : groupData.groupID != null) return false;
-        return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = groupID != null ? groupID.hashCode() : 0;
-        result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "groupID='" + groupID + '\'' +
-                ", groupName='" + groupName + '\'' +
-                '}';
-    }
-
-    public String getGroupID() {
+    public int getGroupID() {
         return groupID;
     }
 
     public GroupData(String groupName, String groupHeader, String groupFooter) {
-        this.groupID = null;
+        this.groupID = 0;
         this.groupName = groupName;
         this.groupHeader = groupHeader;
         this.groupFooter = groupFooter;
     }
 
-    public GroupData(String groupID, String groupName, String groupHeader, String groupFooter) {
+    public GroupData(int groupID, String groupName, String groupHeader, String groupFooter) {
         this.groupID = groupID;
         this.groupName = groupName;
         this.groupHeader = groupHeader;
@@ -60,5 +35,35 @@ public class GroupData {
 
     public String getGroupFooter() {
         return groupFooter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (groupID != groupData.groupID) return false;
+        return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
+    }
+
+    public void setGroupID(int groupID) {
+        this.groupID = groupID;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = groupID;
+        result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "groupID='" + groupID + '\'' +
+                ", groupName='" + groupName + '\'' +
+                '}';
     }
 }
--- addressbook-web-tests/src/test/java/ru/disanger/learning/addressbook/tests/groupTests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/disanger/learning/addressbook/tests/groupTests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.*;
 import ru.disanger.learning.addressbook.model.GroupData;
 import ru.disanger.learning.addressbook.tests.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,12 +14,20 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("Test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    GroupData group = new GroupData(before.get(0).getGroupID(), "Test1", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(group);
+    int max = 0;
+    for (GroupData g : after) {
+      if (g.getGroupID() > max) {
+        max = g.getGroupID();
+      }
+    }
+    group.setGroupID(max);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
